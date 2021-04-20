6a29e6b5ec6479ed45123b910ba01bb3cd6d0386
JesusEMH <47198161+JesusEMH@users.noreply.github.com>
2021-04-19 21:44:51 -0500

    curriculum modificado

--- hooks/useUtils.js
+++ hooks/useUtils.js
@@ -1,6 +1,6 @@
 export default function useUtils(){
     return{
-        curriculum: "https://drive.google.com/file/d/1Nla-Mws6fA3OeXa9bzBBogtFWyCng_4G/view?usp=sharing",
+        curriculum: "https://drive.google.com/file/d/1kArWFxXj2gx-dUtGMzqomhpSQbwgru9u/view?usp=sharing",
         frase: "“Transformar mis ideas a codigo es la mayor satisfaccion que puede haber”"
     }
 }
